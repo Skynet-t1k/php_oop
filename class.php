3f09c41474288cb26e7a16f68ac9c864d353c91f
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-05-26 16:22:08 +0200

    completato selfwork oop5 file class.php

---
+++ class.php
@@ -0,0 +1,63 @@
+<?php
+// Crea un file chiamato class.php e crea una classe astratta di tipo Categoria , con i relativi figli (ovvero che estendono Category) seguendo questa struttura:
+// Category
+// -- Attualita'
+// -- Sport
+// -- Gossip
+// -- Storia
+
+// Tutte le classi dovranno avere un metodo astratto chiamato: getMyCategory() che mostri a schermo il valore relativo.
+
+abstract class Category
+{
+    abstract public function getMyCategory();
+}
+
+class Attualita extends Category
+{
+
+    public function getMyCategory()
+    {
+        echo "Attualità \n";
+    }
+}
+
+class Sport extends Category
+{
+
+    public function getMyCategory()
+    {
+        echo "Sport \n";
+    }
+}
+
+class Gossip extends Category
+{
+
+    public function getMyCategory()
+    {
+        echo "Gossip \n";
+    }
+}
+
+class Storia extends Category
+{
+
+    public function getMyCategory()
+    {
+        echo "Storia \n";
+    }
+}
+
+
+$attualita = new Attualita();
+$attualita->getMyCategory();
+
+$sport = new Sport();
+$sport->getMyCategory();
+
+$gossip = new Gossip();
+$gossip->getMyCategory();
+
+$storia = new Storia();
+$storia->getMyCategory();
