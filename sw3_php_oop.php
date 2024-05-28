975f2313b98b6f35ff0bd166102432b9d19e2bb2
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-05-28 11:58:55 +0200

    per favore legga i commenti, non c'e' un modo per comunicare piu' efficacemente

--- sw3_php_oop.php
+++ sw3_php_oop.php
@@ -1,5 +1,27 @@
 <?php
 
+// crea una struttura a classi sfruttando l’ereditarieta' e seguendo queste semplici regole:
+
+// le classi non devono avere attributi;
+// ogni classe avra' un metodo specifico protected per stampare la sua specializzazione;
+
+
+// ************* non puoi realizzare metodi definiti public per stampare il risultato; ************
+//**************** l’unico metodo public ammesso e' il costrutture. *******************************
+
+// Il risultato atteso sara':
+
+// $magikarp = new Fish();
+//Nel terminale visaulizzerete:
+// Sono un animale Vertebrato
+// Sono un animale a Sangue Freddo
+// Splash!
+
+// Ho visto la videosoluzione ed e' uguale al codice sotto,
+// potrei fare come da lei suggerito ma non so come usare ad es "$mammifero->printSpecialization()" con una protected function 
+// visto che non posso creare public function al di fuori del costruttore.
+// in qualsiasi caso violerei una regola da voi imposta.
+
 abstract class Vertebrates
 {
     public function __construct()
@@ -103,7 +125,6 @@ class Amphibian extends ColdBlooded
         echo "Vivo bene sia a terra che in acqua! \n";
     }
 }
-
 
 $uccello = new Bird;
 $mammifero = new Mammal;
